3bbcbaa4cd4880e0e248bcd575c5a66deb90da32
arakhmatov <arakhmatov@ngxinteractive.com>
2022-03-10 21:05:13 -0800

    mikro-orm setup

---
+++ dist/mikro-orm.configs.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Post_1 = require("./entities/Post");
+const constants_1 = require("./constants");
+exports.default = {
+    entities: [Post_1.Post],
+    dbName: 'lireddit',
+    type: 'postgresql',
+    debug: !constants_1.__prod__,
+};
+//# sourceMappingURL=mikro-orm.configs.js.map
